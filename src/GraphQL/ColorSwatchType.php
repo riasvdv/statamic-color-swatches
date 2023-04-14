56d865b7c05b51dcced9c6218d59b4b0f64857e1
Harvey Massey-Ayling <harvey@lightflows.co.uk>
2023-04-14 11:32:33 +0100

    Add missing graphql type

---
+++ src/GraphQL/ColorSwatchType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Rias\ColorSwatches\GraphQL;
+
+use GraphQL\Type\Definition\Type;
+
+class ColorSwatchType extends \Rebing\GraphQL\Support\Type
+{
+    protected $attributes = [
+        'name' => 'ColorSwatchType',
+        'description' => 'Type for Rias statamic color swatch',
+    ];
+
+    public function fields(): array
+    {
+        return [
+            'label' => [
+                'type' => Type::string(),
+                'description' => 'Color label',
+                'resolve' => function ($values, array $args) {
+                    return $values['label'];
+                }
+            ],
+            'value' => [
+                'type' => Type::string(),
+                'description' => 'Color value',
+                'resolve' => function ($values, array $args) {
+                    return $values['value'];
+                }
+            ],
+        ];
+    }
+}
